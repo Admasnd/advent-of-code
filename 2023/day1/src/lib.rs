c9bfaf8c3fcf4c3b1cd9af09c38b3929af60560b
Antwane Mason <git@aimai.simplelogin.com>
2024-09-27 12:31:33 -0400

    WIP Fixing day1::parse_calibration_line docs
    I have a code usage example that passes `cargo test` for its
    corresponding doctest. However, the full documentation for the
    function, including the usage example itself, does not show up
    in the html page generated from `cargo doc --open`.

--- 2023/day1/src/lib.rs
+++ 2023/day1/src/lib.rs
@@ -4,14 +4,20 @@ use regex::Regex;
 // and two digits.
 
 // calibration data: file where each line contains a first and last digit
-// Given a compiled regex object and a string, return a two digit integer consisting of the first
-// and last digit within the string if the digits can be found.
-// The string represents a line of calibration data.
-// u8 chosen because the maximum number possible is 99, which is less than 255.
-// The regex object is given as input because compilation is expensive.
-// parse_calibration_line("treb7uchet") -> 77
-// parse_calibration_line("1abc2") -> 12
-fn parse_calibration_line(re: Regex, line: String) -> Option<u8> {
+/// Given a compiled regex object and a string, return a two digit integer consisting of the first
+/// and last digit within the string if the digits can be found.
+///
+/// The string represents a line of calibration data.
+/// u8 chosen because the maximum number possible is 99, which is less than 255.
+/// The regex object is given as input because compilation is expensive.
+///
+/// Here is an example of how the function works.
+/// ``` 
+/// # use regex::Regex;
+/// let re = Regex::new(r"^\D*(\d)\D*(\d?)\D*$").unwrap();
+/// assert_eq!(day1::parse_calibration_line(re,String::from("1abc2")), Some(12))
+/// ```
+pub fn parse_calibration_line(re: Regex, line: String) -> Option<u8> {
     // find the first match of the first and last digit
     let (_,[first,last]) = re.captures(&line)?.extract();
     // return two digit number
