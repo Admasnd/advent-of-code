eb2240bdebf383aa39c89046196ae48666163d18
Antwane Mason <git@aimai.simplelogin.com>
2024-10-01 01:14:10 -0400

    Added part1 of day1 Solution

--- 2023/day1/src/bin/part1.rs
+++ 2023/day1/src/bin/part1.rs
@@ -1,3 +1,15 @@
+use regex::Regex;
+use std::fs::File;
+use std::io::BufReader;
+use day1::sum_calibration_lines;
+
 fn main() {
-    println!("Hello, world!");
+    let re = Regex::new(r"^\D*(\d).*?(\d?)\D*$").expect("regex should compile");
+    let f = File::open("input.txt").expect("calibration data should be available on filesystem");
+    let reader = BufReader::new(f);
+    let sum = sum_calibration_lines(&re,reader);
+    match sum {
+        Some(sum) => println!("The sum is {}", sum),
+        None => println!("Issue parsing calibration lines"),
+    }
 }
